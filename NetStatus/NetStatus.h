b4700555b87678c25408c4ad8f688a05281d258c
李懿哲 <liyizhezhe@gmail.com>
2019-08-02 23:45:17 +0800

    Initial Commit

---
+++ NetStatus/NetStatus.h
@@ -0,0 +1,19 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by 李懿哲 on 2019/8/2.
+//  Copyright © 2019 zanyzephyr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
